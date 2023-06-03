4a3d65483fb90a88581b91281c2a3857d118a4fc
Abhinav Acharya <abhinavacharya486@gmail.com>
2023-06-03 21:33:57 +0545

    updated wsgi.py

--- typrrr/wsgi.py
+++ typrrr/wsgi.py
@@ -8,6 +8,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typrrr.settings")
 application = get_wsgi_application()
 app = application
